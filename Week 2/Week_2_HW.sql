2836a97eadacfd98114977eda0b8bb9853f3f4c9
bbollinger2 <bbollinger@csi.edu>
2026-08-31 08:23:38 -0600

    Update Week_2_HW.sql with notes and questions
    Added notes on DISTINCT command and ID number handling.

--- Week 2/Week_2_HW.sql
+++ Week 2/Week_2_HW.sql
@@ -23,3 +23,9 @@ LINE 4: ('The Bodyguard', 'Whitney Houston', 45000000, '1992 11 17')...
 
 SQL state: 42601
 Character: 174
+
+--Notes
+--1. I noticed a new concept called DISTINCT this command is how you seaarch for unique values such as misspelled words or one off inputs this is usefull in finding typoes or other mistakes.
+
+--2. The question I have is when dealling with large amounts of data it's best practice to use a unique id number, but when dealing with colosal amounts of data should you separtae the id numbers like 1-2 milllion are 
+--one thing while 3-4 million are another just so the data can be processes faster or since its numbers does it not matter?
